aa75eaa7174ee79a66d7c67d655bd91e34e37aa4
marilenasantarossa <marilena.santarossa04@gmail.com>
2025-04-15 11:33:00 +0200

    analisi mari

--- mari/analisi.py
+++ mari/analisi.py
@@ -21,6 +21,7 @@ v_fs = np.array(df['Scala_V'].values)
 # phi_fs = 2*np.pi*f*np.array(data[5])*1e-6/(np.pi/2.)
 Vin_errL = v_fs[0]/10*0.41
 V_errL = v_fs/10*0.41
+s_A=np.sqrt((0.041*v_fs/v_in)**2+(0.04*v_fs/v_out)**2)
 print(f"phi={phi}")
 
 plt.hlines(1/np.sqrt(2), color='red', xmin=np.min(f), xmax=np.max(f), linestyles='--', label='1/sqrt(2)')
@@ -40,21 +41,23 @@ inizio= 12
 fine= 27
 
 f_lin=f[inizio: fine]
-phi_lin=phi[inizio: fine]
+f_lin1=f[inizio: (fine-2)]
+phi_lin=phi[inizio: (fine-2)]
 A_lin=A[inizio: fine]
-s_A=np.sqrt((0.041*v_fs/v_in)**2+(0.04*v_fs/v_out)**2)
 s_A_lin=s_A[inizio: fine]
-s_phi_lin=5*1e-6*2*np.pi*f_lin/10*0.41*np.sqrt(2) 
+s_phi_lin=5*1e-6*2*np.pi*f_lin1/10*0.41*np.sqrt(2)/(np.pi/2) 
+print(f"s_phi_lin={s_phi_lin}")
 
 
 def fitlin(x, m, q):
     return m*x + q
 
-popt1, ppcov1=curve_fit(fitlin, f_lin, phi_lin, sigma=s_phi_lin)
+popt1, ppcov1=curve_fit(fitlin, f_lin1, phi_lin, sigma=s_phi_lin)
 popt, ppcov=curve_fit(fitlin, f_lin, A_lin, sigma=s_A_lin)
 f_fit=np.linspace(min(f_lin), max(f_lin), 15)
+f_fit1=np.linspace(min(f_lin1), max(f_lin1), 13)
 A_fit=fitlin(f_fit, *popt)
-phi_fit=fitlin(f_fit, *popt1)
+phi_fit=fitlin(f_fit1, *popt1)
 
 residui=A_lin-A_fit
 residui1=phi_lin-phi_fit
@@ -102,16 +105,16 @@ delta_T=np.trunc(delta_T_grezzo).astype(int)
 print(f"scala fasi:{delta_T}")
 
 fig, (bx1, bx2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [3, 1]}, figsize=(8,6))
-bx1.scatter(f_lin, phi_lin, label='phi=2pi*f*deltaT')
-bx1.errorbar(f_lin, phi_lin, yerr=s_phi_lin, fmt='o')
+bx1.scatter(f_lin1, phi_lin, label='phi=2pi*f*deltaT')
+bx1.errorbar(f_lin1, phi_lin, yerr=s_phi_lin, fmt='o')
 bx1.axhline(0.5, color='green', linestyle='--', label='phi=0.5') 
 bx1.set_ylabel('phi')
 bx2.set_xlabel('f [Hz]')
 bx1.legend()
-bx1.plot(f_fit, phi_fit, label='fit lineare' )
+bx1.plot(f_fit1, phi_fit, label='fit lineare' )
 bx1.grid(True)
 
-bx2.errorbar(f_lin, residui1, yerr=s_phi_lin, fmt='o')
+bx2.errorbar(f_lin1, residui1, yerr=s_phi_lin, fmt='o')
 bx2.axhline(0, c='red', linestyle='-')
 bx2.grid(True)
 
@@ -126,6 +129,42 @@ print(f'freq_taglio={f_tlin1} +- {s_ft1}')
 bx1.scatter(f_tlin1, 0.5 , c='green', label='frequenza di taglio: ft=(5500+-300)Hz' )
 bx1.errorbar(f_tlin1, 0.5, xerr=s_ft1, capsize= True)
 bx1.legend()
+
+plt.tight_layout(pad=0.2)
+plt.show()
+
+fig, (cx1, cx2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [3, 1]}, figsize=(8,6))
+cx1.scatter(f_lin, A_lin, c="blue", label='A=V_out/V_in')
+cx1.errorbar(f_lin, A_lin, yerr=s_A_lin, c="blue", fmt='o')
+cx1.axhline(1/np.sqrt(2), color='green', linestyle='--', label='A=1/sqrt(2)') 
+cx1.set_ylabel('A')
+cx2.set_xlabel('f [Hz]')
+cx1.legend()
+cx1.plot(f_fit, A_fit, c="blue", label='fit lineare' )
+cx1.grid(True)
+
+cx2.errorbar(f_lin, residui, yerr=s_A_lin, fmt='o')
+cx2.axhline(0, c='yellow', linestyle='-')
+cx2.grid(True)
+
+cx1.scatter(f_lin1, phi_lin, c="orange", label='phi=2pi*f*deltaT')
+cx1.errorbar(f_lin1, phi_lin, c="orange", yerr=s_phi_lin, fmt='o')
+cx1.axhline(0.5, color='red', linestyle='--', label='phi=0.5') 
+cx1.set_ylabel('phi')
+cx2.set_xlabel('f [Hz]')
+cx1.legend()
+cx1.plot(f_fit1, phi_fit, c="orange", label='fit lineare' )
+cx1.grid(True)
+
+cx2.errorbar(f_lin1, residui1, yerr=s_phi_lin, fmt='o')
+cx2.axhline(0, c='yellow', linestyle='-')
+cx2.grid(True)
+
+cx1.scatter(f_tlin, 1/np.sqrt(2), c='green', label='da A: ft=(5180+-100)Hz' )
+cx1.errorbar(f_tlin, 1/np.sqrt(2), c='green', xerr=s_ft, capsize= True)
+cx1.scatter(f_tlin1, 0.5 , c='red', label='da phi: ft=(5500+-300)Hz' )
+cx1.errorbar(f_tlin1, 0.5, c='red', xerr=s_ft1, capsize= True)
+cx1.legend()
 
 plt.tight_layout(pad=0.2)
 plt.show()
