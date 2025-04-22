691208b259abc7e45df19f46690b424d674711de
gretaaspano <spano.greta0@gmail.com>
2025-04-22 12:18:16 +0200

    modifiche s_A

--- gre/fit_non_lin.py
+++ gre/fit_non_lin.py
@@ -28,7 +28,21 @@ scala_us = df['Scala_us'].values * 1e-6
 Vin_err = scala_V[0] / 10 * 0.41
 Vout_err = scala_V / 10 * 0.41
 fase_err = 2 * np.pi * frequenze * scala_us / 10 * 0.41 * np.sqrt(2) / (np.pi / 2)
-sigma_A = A * np.sqrt((0.04 * scala_V / Vin)**2 + (0.04 * scala_V / Vout)**2)
+#sigma_A = A * np.sqrt((0.04 * scala_V / Vin)**2 + (0.04 * scala_V / Vout)**2)
+#sigma_A = A * np.sqrt((0.041 / Vin)**2 + (0.041 * scala_V / Vout)**2 + (1.2/100 * Vin)**2 + (1.2/100 * Vout)**2)
+
+low_freq = frequenze < 2100           
+high_freq = frequenze > 10000          
+mid_freq = ~(low_freq | high_freq)  
+sigma_A = np.zeros_like(A)
+
+sigma_A_ext = A * np.sqrt((0.041 / Vin)**2 + (0.041 * scala_V / Vout)**2 + (1.2/100 * Vin)**2 + (1.2/100 * Vout)**2)
+sigma_A_mid = A * np.sqrt((0.041 / Vin)**2 + (0.041 * scala_V / Vout)**2)
+
+sigma_A[low_freq] = sigma_A_ext[low_freq]
+sigma_A[high_freq] = sigma_A_ext[high_freq]
+sigma_A[mid_freq] = sigma_A_mid[mid_freq]
+
 
 #FIT NON LINEARE
 f_fitspace = np.linspace(min(frequenze), max(frequenze), 10000)
@@ -62,19 +76,20 @@ modello_phi_lower = fit_fase(f_fitspace, ft_phi - ft_phi_err)
 #GRAFICO
 plt.figure(figsize=(10, 6))
 plt.axhline(1 / np.sqrt(2), color='red', linestyle='--', label='1/√2')
+plt.axhline(0.5, color='brown', linestyle='--', label='π/4')
 plt.errorbar(frequenze, A, yerr=sigma_A, fmt='o', ms=3, color='purple', label='Attenuazione A')
-plt.plot(f_fitspace, modello_A, color='orange', label='Fit attenuazione')
+plt.plot(f_fitspace, modello_A, color='orange', label=(f"Fit A: ft= ({ft_fit:.2f} ± {ft_err:.2f}) Hz"))
 plt.fill_between(f_fitspace, modello_A_lower, modello_A_upper, color='orange', alpha=0.2, label='Incertezza fit A')
 
 plt.errorbar(frequenze, fase_norm, yerr=fase_err, fmt='o', ms=3, color='green', label='Fase φ normalizzata')
-plt.plot(f_fitspace, modello_phi, color='blue', label='Fit fase')
+plt.plot(f_fitspace, modello_phi, color='blue', label=(f"Fit φ: ft_phi= ({ft_phi:.2f} ± {ft_phi_err:.2f}) Hz"))
 plt.fill_between(f_fitspace, modello_phi_lower, modello_phi_upper, color='blue', alpha=0.2, label='Incertezza fit φ')
 
 plt.xscale('log')
-plt.xlabel('Frequenza [Hz]')
+plt.xlabel('log10(f) [Hz]')
 plt.ylabel('A [V/V] con φ-->1')
-plt.title('Fit non lineare attenuazione e fase circuito RC')
-plt.grid(True, which='both', linestyle='--', alpha=0.6)
+#plt.title('Fit non lineare attenuazione e fase circuito RC')
+plt.grid(True, which='both', linestyle='--', alpha=0.3)
 plt.legend()
 plt.tight_layout()
 plt.show()
